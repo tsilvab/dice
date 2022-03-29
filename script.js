cd27b24bf17fba4e1d7de8bcca1f0d33f79ec340
Thefany Silva <t.silvabel@Thefanys-MacBook-Pro.local>
2022-03-28 21:37:41 -0400

    js code for random dice roll

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-var randomNumber1 = math.floor(math.random()* 6)+1;
+//dice on left
+var randomNumber1 = Math.floor(Math.random() * 6) + 1; //random number 1-6
+
+var randomDiceImage = "Dice" + randomNumber1 + ".png"; //dice1.png-dice6.png
+
+var randomImageSource = "images/" + randomDiceImage; //images/dice1.png-images/dice6.png
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource);
+
+//dice on right, also code combined to less lines of code
+var randomNumber2 = Math.floor(Math.random() * 6) + 1; 
+
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+var image2 = document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+//if player 1 wins
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "🚩Player 1 Wins!";
+}
+
+//if player 2 wins
+else if (randomNumber2 > randomNumber1) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins!🚩";
+}
+//tie
+else {
+    document.querySelector("h1").innerHTML = "Draw!"
+}
