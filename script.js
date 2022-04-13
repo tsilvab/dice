1e9c8e6eb38880cd6e3718e1dc7919c6a25bf043
Thefany Silva <t.silvabel@Thefanys-MBP.fios-router.home>
2022-04-13 14:52:37 -0400

    declaration of variables

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 function diceRoll(){
 //dice on left
-var randomNumber1 = Math.floor(Math.random() * 6) + 1; //random number 1-6
+let randomNumber1 = Math.floor(Math.random() * 6) + 1; //random number 1-6
 
-var randomImageSource = "images/dice" + randomNumber1 + ".png"; //images/dice1.png-images/dice6.png
+let randomImageSource = "images/dice" + randomNumber1 + ".png"; //images/dice1.png-images/dice6.png
 
-var image1 = document.querySelectorAll("img")[0];
+let image1 = document.querySelectorAll("img")[0];
 
 image1.setAttribute("src", randomImageSource);
 
 //dice on right, also code combined for less lines of code
-var randomNumber2 = Math.floor(Math.random() * 6) + 1; 
+let randomNumber2 = Math.floor(Math.random() * 6) + 1; 
 
-var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+let randomImageSource2 = "images/dice" + randomNumber2 + ".png";
 
-var image2 = document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
 //if player 1 wins
 if (randomNumber1 > randomNumber2) {
